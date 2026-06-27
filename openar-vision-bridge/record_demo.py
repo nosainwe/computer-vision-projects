878846a9813ff6ca86698825a5a6864bed95e83b
Nosa Inwe <56210353+nosainwe@users.noreply.github.com>
2026-06-28 00:37:15 +0300

    Update record_demo.py

--- openar-vision-bridge/record_demo.py
+++ openar-vision-bridge/record_demo.py
@@ -1,7 +1,7 @@
 """
 record_demo.py
 --------------
-This generates a short demo of the virtual 128×64 OLED HUD as:
+This generates a short demo of the virtual 128×64 OLED HUD:
 - demo_oled.mp4
 - demo_oled.gif
 
